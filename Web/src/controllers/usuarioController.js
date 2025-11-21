267fe77bd338faad0cdd8a214117c141c10fb935
Strilicherk <matheuscherk@hotmail.com>
2025-11-21 02:34:34 -0300

    Login finalizado.

--- Web/src/controllers/usuarioController.js
+++ Web/src/controllers/usuarioController.js
@@ -5,11 +5,10 @@ function autenticar(req, res) {
     var senha = req.body.senhaServer;
     
     if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
+        res.status(404).send("Email não encontrado.");
     } else if (senha == undefined) {
-        res.status(400).send("Sua senha está indefinida!");
+        res.status(404).send("Senha não encontrada.");
     } else {
-        
         usuarioModel.autenticar(email, senha)
         .then(
             function (resultadoAutenticar) {
@@ -19,24 +18,16 @@ function autenticar(req, res) {
                 if (resultadoAutenticar.length == 1) {
                     console.log(resultadoAutenticar);
                     
-                    aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                    .then((resultadoAquarios) => {
-                        if (resultadoAquarios.length > 0) {
-                            res.json({
-                                id: resultadoAutenticar[0].id,
-                                email: resultadoAutenticar[0].email,
-                                nome: resultadoAutenticar[0].nome,
-                                senha: resultadoAutenticar[0].senha,
-                                aquarios: resultadoAquarios
-                            });
-                        } else {
-                            res.status(204).json({ aquarios: [] });
+                    res.status(200).json(
+                        {
+                            id: resultadoAutenticar[0].ID_USUARIO,
+                            nickname: resultadoAutenticar[0].NICKNAME
                         }
-                    })
+                    );
                 } else if (resultadoAutenticar.length == 0) {
-                    res.status(403).send("Email e/ou senha inválido(s)");
+                    res.status(404).send("Email e/ou senha inválido(s)");
                 } else {
-                    res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+                    res.status(409).send("Ocorreu um erro inesperado. Tente novamente mais tarde.");
                 }
             }
         ).catch(
@@ -47,7 +38,6 @@ function autenticar(req, res) {
             }
         );
     }
-    
 }
 
 function cadastrar(req, res) {
--- Web/src/models/usuarioModel.js
+++ Web/src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT id, nome, email, fk_empresa as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT ID_USUARIO, NICKNAME FROM USUARIO WHERE EMAIL = '${email}' AND SENHA = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -13,10 +13,8 @@ function autenticar(email, senha) {
 function cadastrar(nickname, email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nickname, email, senha);
     
-    // Insira exatamente a query do banco aqui, lembrando da nomennclatura exata nos valores
-    //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO USUARIO (nickname, email, senha) VALUES ('${nickname}', '${email}', '${senha}');
+        INSERT INTO USUARIO (NICKNAME, EMAIL, SENHA) VALUES ('${nickname}', '${email}', '${senha}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
