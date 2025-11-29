d597d56198849d83c90256d47fc8534365ae1ef1
Strilicherk <matheuscherk@hotmail.com>
2025-11-29 01:14:00 -0300

    Implementada função na API que formata o JSON recebido para tirar a sua redundância excessiva.

--- Web/src/controllers/bandaController.js
+++ Web/src/controllers/bandaController.js
@@ -15,16 +15,17 @@ function listar(req, res) {
 }
 
 function buscarPorId(req, res) {
-    var id = req.params.idBanda;
+    var idBanda = req.params.idBanda;
+    var idUsuario = req.query.idUsuario;
     
-    if (id == '' || id == undefined) {
-        console.log(`Id da banda vazio ou indefinido: ${id}`)
+    if (!idBanda || !idUsuario) {
+        console.log(`Id da banda ou do usuário invalidos: ${idBanda} | ${idUsuario}`)
     } else {
-        bandaModel.buscarPorId(id).then(function (resultado) {
+        bandaModel.buscarPorId(idBanda, idUsuario).then(function (resultado) {
             if (resultado.length <= 0) {
-                res.status(404).json(resultado)
+                res.status(404).json(resultado);
             } else {
-                res.status(200).json(resultado)
+                res.status(200).json(formatarJson(resultado));
             }
         }).catch(function (erro) {
             console.log(erro);
@@ -33,6 +34,54 @@ function buscarPorId(req, res) {
         })
     }
 }
+
+function formatarJson(resultado) {
+    
+    var bandaInfos = {
+        band_id: resultado[0].band_id, 
+        band: resultado[0].band, 
+        genre: resultado[0].genre, 
+        about: resultado[0].about, 
+        banner_path: resultado[0].band_banner_path,
+        
+        albums: []
+    }
+    
+    console.log('Somente banda', bandaInfos)
+    
+    for (var i = 0; i < resultado.length; i++) {
+        var albumExistente = bandaInfos.albums.find(function (album) {
+            return album.album_id == resultado[i].album_id;
+        })
+        
+        if (albumExistente) {
+            albumExistente.musics.push( {
+                music_id: resultado[i].music_id,
+                music: resultado[i].music,
+                mood: resultado[i].mood,
+                color: resultado[i].color,
+            })   
+        } else {
+            bandaInfos.albums.push( {
+                album_id: resultado[i].album_id,
+                album: resultado[i].album,
+                album_release_date: resultado[i].album_release_date,
+                album_cover_path: resultado[i].album_cover_path,
+                
+                musics: [{
+                    music_id: resultado[i].music_id,
+                    music: resultado[i].music,
+                    mood: resultado[i].mood,
+                    color: resultado[i].color,
+                }]
+            })
+        }
+    }
+    
+    console.log('Pós formatação', bandaInfos)
+    return bandaInfos;
+}
+
 
 module.exports = {
     listar,
--- Web/src/models/bandaModel.js
+++ Web/src/models/bandaModel.js
@@ -5,15 +5,29 @@ function listar() {
     var instrucaoSql = `
         SELECT id, name, cover_path FROM band ORDER BY name;
     `;
-
+    
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarPorId(id) {
+function buscarPorId(idBanda, idUsuario) {
     var instrucaoSql = `
-        SELECT * FROM vw_band_album_music WHERE id = ${id};
+        SELECT 
+            vw.*,
+            IF(ml.user_id IS NOT NULL, 1, 0) AS is_liked,
+            IF(vw.band_id = u.band_id, 1, 0) AS is_favorite_band,
+            IF(vw.music_id = u.music_id, 1, 0) AS is_favorite_music
+        FROM
+            vw_band_album_music AS vw
+                LEFT JOIN
+            music_like ml ON vw.music_id = ml.music_id
+                AND ml.user_id = ${idUsuario}
+                LEFT JOIN
+            user u ON u.id = ${idUsuario}
+                WHERE vw.band_id = ${idBanda};
     `
+    
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
